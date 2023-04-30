f59bb3bd62a6812139b2fe07d811c69924516e9a
Matej Grznar <xgrznarm@stuba.sk>
2023-04-30 17:18:42 +0200

    prepojenie Arduino UNO

--- WebApp/semestralka.py
+++ WebApp/semestralka.py
@@ -68,5 +68,53 @@ def readDB():
             cursor.close()
             cnx.close()
 
+def saveFile(data, filename):
+    try:
+        with open(filename, 'a') as f:
+            f.write(json.dumps(data) + '\n')
+
+        print(f"Successfully saved data to {filename}")
+    except IOError as e:
+        print(f"Error saving data to {filename}: {e}")
+
+def readFile(filename):
+    try:
+        with open(filename, 'r') as f:
+            data = f.read().splitlines()
+        print(f"Successfully read data from {filename}")
+        return data
+    except IOError as e:
+        print(f"Error reading data from {filename}: {e}")
+        return None
+
+def background_thread(args, ms=''):
+    last = ""
+    data = ""
+    count = 0
+    arduino = serial.Serial('/dev/ttyS0', baudrate=115200)
+    if ms != '':
+        if(ms and ms != last):
+            arduino.write(ms.encode())
+            last = ms
+            print('EMIT CALL: ' + ms) 
+    else:    
+        while True:
+            received_message = arduino.readline().decode();
+            data_list = []
+            print(received_message)
+            if(received_message):
+                count = count + 1
+                try:
+                    data = json.loads(received_message)
+                    data_list.append(data)
+                    print(data)
+                    saveDB(data)
+                    saveFile(data, "output_data")
+                    json_object = readFile("output_data")
+                    socketio.emit('my_response', {'data': json_object[-1], 'count': count }, namespace='/test')
+                except json.JSONDecodeError:
+                    pass   
+        #arduino.close()            
+
 if __name__ == '__main__':
     socketio.run(app, host="0.0.0.0", port=80, debug=True)    
